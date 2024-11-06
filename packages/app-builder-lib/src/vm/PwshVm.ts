7c945d9fc99648177cc1def1df752820228f3bf2
Mike Maietta <mmaietta@outlook.com>
2024-11-06 10:06:54 -0800

    remove comment

--- packages/app-builder-lib/src/vm/PwshVm.ts
+++ packages/app-builder-lib/src/vm/PwshVm.ts
@@ -12,7 +12,6 @@ export class PwshVmManager extends VmManager {
     if (await isPwshAvailable.value) {
       return "pwsh"
     }
-    // We don't install `powershell` in our default `wine` docker image as it adds 186mb to the image and currently is only needed for Azure Trusted Signing
     const errorMessage = `unable to find \`pwsh\` within docker container, please install per https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-linux`
     log.error(null, errorMessage)
     throw new Error(errorMessage)
